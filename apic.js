af6319119ebeb201f2976a9e6619847882d8da29
Scott Ballantyne <ussballantyne@gmail.com>
2017-11-10 19:51:37 -0600

    initial commit

---
+++ apic.js
@@ -0,0 +1,100 @@
+// These are the ids that go along with the different image types, I haven't tried to 
+// the ones with letters yet, if that doesn't work maybe this should just be the index 
+// of the array on this page here.
+//
+// https://github.com/Zazama/node-id3/blob/master/index.js#L78-L100
+//
+// If you find that it isn't working as expected, let me know and I will adjust the code.
+//
+// $00     Other
+// $01     32x32 pixels 'file icon' (PNG only)
+// $02     Other file icon
+// $03     Cover (front)
+// $04     Cover (back)
+// $05     Leaflet page
+// $06     Media (e.g. label side of CD)
+// $07     Lead artist/lead performer/soloist
+// $08     Artist/performer
+// $09     Conductor
+// $0A     Band/Orchestra
+// $0B     Composer
+// $0C     Lyricist/text writer
+// $0D     Recording Location
+// $0E     During recording
+// $0F     During performance
+// $10     Movie/video screen capture
+// $11     A bright coloured fish
+// $12     Illustration
+// $13     Band/artist logotype
+// $14     Publisher/Studio logotype
+
+module.exports = function(id) {
+  switch(id) {
+    case 0:
+      return {id: 0, name: 'Other'};
+
+    case 1:
+      return {id: 1, name: 'file icon'};
+    
+    case 2:
+      return {id: 2, name: 'Other file icon'};
+    
+    case 3:
+      return {id: 3, name: 'Cover (front)'};
+ 
+    case 4:
+      return {id: 4, name: 'Cover (back)'};
+
+    case 5:
+      return {id: 5, name: 'Leaflet page'};
+    
+    case 6:
+      return {id: 6, name: 'Media'};
+    
+    case 7:
+      return {id: 7, name: 'Lead Artist/lead performer/soloist'};
+     
+    case 8:
+      return {id: 8, name: 'Artist/performer'};
+
+    case 9:
+      return {id: 9, name: 'Conductor'};
+    
+    case "A":
+      return {id: 10, name: 'Band/Orchestra'};
+    
+    case "B":
+      return {id: 11, name: 'Composer'};
+     
+    case "C":
+      return {id: 12, name: 'Lyracist/text writer'};
+
+    case "D":
+      return {id: 13, name: 'Recording Location'};
+    
+    case "E":
+      return {id: 14, name: 'During Recording'};
+
+    case "F":
+      return {id: 14, name: 'During Performance'};
+
+    case 10:
+      return {id: 10, name: 'Movie/video screen capture'};
+    
+    case 11:
+      return {id: 11, name: 'A bright coloured fish'};
+     
+    case 12:
+      return {id: 12, name: 'Illustration'};
+
+    case 13:
+      return {id: 13, name: 'Band/artist logotype'};
+    
+    case 14:
+      return {id: 14, name: 'Publisher/Studio logotype'};
+    
+    default: 
+      return {id: 3, name: 'Cover Front'};
+  }
+}
+
---
+++ index.js
@@ -0,0 +1,96 @@
+const lame         = require("lame");
+const fs           = require('fs');
+const randomstring = require('randomstring');
+const NodeID3      = require('node-id3')
+const wav          = require('wav');
+const apic         = require('./apic');
+
+
+module.exports    = function(paperclip) {
+  var obj = {};
+
+  obj.paperclip  = paperclip;
+
+  obj.perform = function(options, next) {
+    var self  = this;
+
+    var supportedMeta = [ 'album','bpm','composer','genre','copyright','date',
+                          'playlistDelay','encodedBy','textWriter','fileType',
+                          'time','contentGroup','title','subtitle','initialKey',
+                          'language','length','mediaType','originalTitle',
+                          'originalFilename','originalTextwriter','originalArtist',
+                          'originalYear','fileOwner','artist','performerInfo',
+                          'conductor','remixArtist','partOfSet','publisher',
+                          'trackNumber','recordingDates','internetRadioName',
+                          'internetRadioOwner','size','ISRC','encodingTechnology',
+                          'year','comment', 'image' ];
+
+    // https://en.wikipedia.org/wiki/ID3#ID3v2_embedded_image_extension
+    // image: { 
+    //   mime: "png/jpeg"/undefined, 
+    //   type: { 
+    //     id: 3,
+    //     name: "front cover
+    //   }, 
+    //   description: "image description", 
+    //   imageBuffer: (file buffer)
+    // }
+
+
+    var tags = {};
+
+    for (i = 0; i < supportedMeta.length; i++) {
+      var tag = supportedMeta[i]
+      if (self.paperclip.document[tag]) {
+        tags[tag] = self.paperclip.document[tag];
+      }
+    }
+    if (self.paperclip.document.comment != undefined) {
+      tags.comment = {
+        language: (self.paperclip.document.language ? self.paperclip.document.language : 'eng'),
+        text: self.paperclip.document.comment
+      }
+    }
+
+    // if (self.paperclip.document.image != undefined) {
+    //   tags.image = {
+    //     mime: self.paperclip.document.image.content_type,
+    //     imageBuffer: self.paperclip.document.image.buffer,
+
+    //   }
+      
+    //   tags.image.type = apic(self.paperclip.image_type)
+      
+    //   if (self.paperclip.document.image_description) {
+    //     tags.image.description = self.paperclip.document.image_description;
+    //   }
+    // }
+
+    tags.encodingTechnology = 'lame';
+
+    var outfile = self.paperclip.file.path + randomstring.generate() +".mp3"
+    input = fs.createReadStream(self.paperclip.file.path);
+    output = fs.createWriteStream(outfile);
+
+    output.on('finish', function() {
+      NodeID3.write(tags, outfile, function(err) {
+        fs.readFile(outfile, function(err, buffer) {
+          fs.unlink(outfile, function(err) {
+            next(null, buffer);
+          });
+        });
+      });
+    })
+
+    var reader = new wav.Reader();
+    reader.on('format', onFormat);
+
+    function onFormat (format) {
+      var encoder = new lame.Encoder(format);
+      reader.pipe(encoder).pipe(output);
+    }
+    input.pipe(reader);
+  }
+  return obj;
+};
+
